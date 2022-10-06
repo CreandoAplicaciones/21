45a2943ae11c4951d8eaa92ebb1b66a5e5d385ec
DESKTOP-U9CG8QT\pedro <pedroperezaragon@gmail.com>
2022-10-06 05:17:16 +0200

    I put top and bottom banner

--- app/src/main/java/com/vegano/en21/dias/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/vegano/en21/dias/ui/view/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.vegano.en21.dias.ui.view.main
 
 import android.os.Bundle
+import com.google.android.gms.ads.*
+import com.vegano.en21.dias.R
 import com.vegano.en21.dias.databinding.ActivityMainBinding
 import com.vegano.en21.dias.ui.base.BaseActivity
 
@@ -15,7 +17,14 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        MobileAds.initialize(this) {}
+
+        val adRequest = AdRequest.Builder().build()
+        binding.adViewSup.loadAd(adRequest)
+        binding.adViewInf.loadAd(adRequest)
     }
+
+
 
 
 }
